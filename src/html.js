2cee8130522616860e415e0cae0cf5c6d8efc240
Arul Valan Anto S <61511921+arulvalananto@users.noreply.github.com>
2023-04-13 18:12:19 +0530

    fix: font cross origin issue

--- src/html.js
+++ src/html.js
@@ -20,6 +20,16 @@ export default function HTML(props) {
                     content="fullstackdeveloper, programmer, portfolio, arulvalananto"
                 />
                 {props.headComponents}
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossOrigin
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Schibsted+Grotesk:ital,wght@0,400;0,500;0,700;1,400&display=swap"
+                    rel="stylesheet"
+                ></link>
             </head>
             <body {...props.bodyAttributes}>
                 {props.preBodyComponents}
