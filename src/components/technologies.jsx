105bc91ca07ecef12dc19b4976ded3f4ed213731
Arul Valan Anto S <61511921+arulvalananto@users.noreply.github.com>
2023-04-13 18:58:13 +0530

    fix: prod issue #2

--- src/components/technologies.jsx
+++ src/components/technologies.jsx
@@ -8,10 +8,10 @@ const Technologies = () => {
         <>
             <Tech
                 tech={tech.React}
-                position={{ top: '100px', left: '200px' }}
+                position={{ top: '140px', left: '555px' }}
             />
             <Tech
-                tech={tech.Mongoose}
+                tech={tech.Stripe}
                 position={{ top: '50px', left: '1000px' }}
             />
             <Tech tech={tech.Git} position={{ top: '0px', left: '1650px' }} />
@@ -20,15 +20,15 @@ const Technologies = () => {
                 position={{ top: '850px', left: '1800px' }}
             />
             <Tech
-                tech={tech.Stripe}
-                position={{ top: '700px', left: '925px' }}
+                tech={tech.Mongoose}
+                position={{ top: '575px', left: '1225px' }}
             />
             <Tech
                 tech={tech.Angular}
                 position={{ top: '250px', left: '1900px' }}
             />
             <Tech
-                tech={tech.NodeJS}
+                tech={tech.PlayWright}
                 position={{ top: '400px', left: '1500px' }}
             />
             <Tech
@@ -36,13 +36,10 @@ const Technologies = () => {
                 position={{ top: '790px', left: '350px' }}
             />
             <Tech
-                tech={tech.PlayWright}
+                tech={tech.NodeJS}
                 position={{ top: '550px', left: '50px' }}
             />
-            <Tech
-                tech={tech.Redis}
-                position={{ top: '280px', left: '500px' }}
-            />
+            <Tech tech={tech.Redis} position={{ top: '10px', left: '50px' }} />
         </>
     );
 };
