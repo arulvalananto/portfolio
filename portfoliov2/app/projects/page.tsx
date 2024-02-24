b34a90c6ceb75b05b045804fcd5bae049e7b2577
arulvalananto <arulvalananto@gmail.com>
2024-02-24 06:14:20 +0530

    update: project demo video

--- portfoliov2/app/projects/page.tsx
+++ portfoliov2/app/projects/page.tsx
@@ -150,6 +150,7 @@ const ProjectsPage = () => (
                 alt="AirDeck Project"
                 width={160}
                 height={40}
+                priority
               />
               <p className="text-2xl font-quicksand font-normal">
                 Document Narration Platform
@@ -158,7 +159,7 @@ const ProjectsPage = () => (
             <div className="w-[450px] h-96 flex items-center justify-center animate-sizeup-slow">
               <video
                 preload="none"
-                src="/projects_airdeck_demo.mp4"
+                src="/projects_airdeck_demo_v2.mp4"
                 autoPlay
                 loop
                 muted
@@ -278,7 +279,7 @@ const ProjectsPage = () => (
             <div className="w-[500px] h-96 scale-90 flex items-center justify-center animate-sizeup-slow">
               <video
                 preload="none"
-                src="/projects_scaffolder_demo.mp4"
+                src="/projects_scaffolder_demo_v2.mp4"
                 autoPlay
                 loop
                 muted
