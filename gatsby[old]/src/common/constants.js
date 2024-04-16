3fd0c56b277e4ecf25cf18097162b5a42754bd23
Arul Valan Anto S <61511921+arulvalananto@users.noreply.github.com>
2024-04-16 22:16:04 +0530

    Update constants.js

--- gatsby[old]/src/common/constants.js
+++ gatsby[old]/src/common/constants.js
@@ -1,6 +1,6 @@
 const constants = {
-    AWS_CLOUNDFRONT: 'https://d2sgkcesfdic4y.cloudfront.net',
-    AWS_CLOUNDFRONT_URL: `https://d2sgkcesfdic4y.cloudfront.net/projects`,
+    AWS_CLOUNDFRONT: '[USE_YOUR_CDN_LINK]',
+    AWS_CLOUNDFRONT_URL: `[USE_YOUR_CDN_LINK]`,
     GIF_TIMEOUT_SECONDS: 1000,
     GOOGLE_SEARCH_URL: `https://www.google.com/search?q=`,
     PROJECTS_SECTION: 'Projects',
